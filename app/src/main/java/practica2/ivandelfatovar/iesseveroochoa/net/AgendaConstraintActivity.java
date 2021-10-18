5ce3406c227209eacac6c94281d65fa3b28768fe
ivand <ivandelfa@gmail.com>
2021-10-18 22:24:09 +0200

    Comienzo con la activity_agenda_constraint.xml

--- app/src/main/java/practica2/ivandelfatovar/iesseveroochoa/net/AgendaConstraintActivity.java
+++ app/src/main/java/practica2/ivandelfatovar/iesseveroochoa/net/AgendaConstraintActivity.java
@@ -4,13 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class AgendaActivity extends AppCompatActivity {
+public class AgendaConstraintActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_agenda);
+        setContentView(R.layout.activity_agenda_constraint);
     }
-
-
 }
--- app/src/main/java/practica2/ivandelfatovar/iesseveroochoa/net/MainActivity.java
+++ app/src/main/java/practica2/ivandelfatovar/iesseveroochoa/net/MainActivity.java
@@ -2,20 +2,30 @@ package practica2.ivandelfatovar.iesseveroochoa.net;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnAceptar;
+    Button btnAgenda;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnAceptar = (Button)findViewById(R.id.btnAgenda);
-        btnAceptar.setOnClickListener(new.);
+        btnAgenda =(Button)findViewById(R.id.btnAgenda);
+
+        btnAgenda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AgendaActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
 
